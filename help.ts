296956b219d1c7513765290ab92e58a7294e8cd2
DanyaalZar-Solirius <danyaal.zar@solirius.com>
2025-03-25 11:57:46 +0000

    Page 3 done

--- help.ts
+++ help.ts
@@ -1,5 +1,11 @@
 import {expect, Page, test} from "@playwright/test";
+import path from "path";
 
 export async function checkCurrentPage(url:string, page:Page) {
     await expect(page).toHaveURL(url);
+}
+
+export async function clickContinueButton(page: Page) {
+    // Clicking the "Continue" button
+    await page.getByText('Continue').click();
 }
