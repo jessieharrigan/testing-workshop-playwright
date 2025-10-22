9aa75a980691a4d94ee758dd38ebb4cf25e13755
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2025-10-22 16:56:44 +0100

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,9 +1,8 @@
 import {expect, test} from "@playwright/test";
 
+// Remame this test
 test(`First test`, async ({ page }) => {
     await page.goto('');
-    await expect(page.locator(`.govuk-heading-xl`))
-        .toHaveText(`Calculate holiday entitlement`);
+    await expect(page.locator(`.govuk-heading-xl`)).toHaveText(`Calculate holiday entitlement`);
     // Continue me!
-    // How would you click the continue button?
 });
